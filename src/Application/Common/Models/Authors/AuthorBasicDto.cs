b062f41f590f8e77b5128f9446fd0b065d4c9a32
oriel <orielarteagaj@gmail.com>
2025-03-25 13:18:25 -0300

    Add TotalPublishedBooks property to AuthorBasicDto and create BookBasicDto class.

--- src/Application/Common/Models/Authors/AuthorBasicDto.cs
+++ src/Application/Common/Models/Authors/AuthorBasicDto.cs
@@ -8,13 +8,15 @@ public class AuthorBasicDto
     public string? Name { get; set; }
     public string? Nationality { get; set; }
     public DateTime Birthday { get; set; }
+    public int TotalPublishedBooks { get; set; }
     
     private class Mapping : Profile
     {
         public Mapping()
         {
-            CreateMap<Author, AuthorBasicDto>();
+            CreateMap<Author, AuthorBasicDto>()
+                .ForMember(dest => dest.TotalPublishedBooks, 
+                    opt => opt.MapFrom(src => src.Books.Count));
         }
     }
 }
-
---
+++ src/Application/Common/Models/Books/BookBasicDto.cs
@@ -0,0 +1,25 @@
+﻿using LibraryApp.Domain.Entities;
+
+namespace LibraryApp.Application.Common.Models.Books;
+
+public class BookBasicDto : BaseDto
+{
+    public string? Title { get; set; }
+    public string? Resume { get; set; }
+    public int PublicationYear { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? ISBN { get; set; }
+    public int Stock { get; set; }
+    public string? Author { get; set; }
+    public string? Genre { get; set; }
+    
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Book, BookBasicDto>()
+                .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.Author != null ? src.Author.Name : null))
+                .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre != null ? src.Genre.Name : null));
+        }
+    }
+}
