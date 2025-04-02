3e74ca8603150ba9b9aaee477c5f7a0f1e870cc9
Oriel Arteaga <oriel.arteaga@alsacia.com.uy>
2025-04-02 08:53:51 -0300

    Fix connection string

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -21,7 +21,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("SqlDbConnectionContainer");
+        var connectionString = configuration.GetConnectionString("SqlDbConnection") ?? 
+            configuration.GetConnectionString("SqlDbConnectionContainer");
 
         Guard.Against.Null(connectionString, message: "Connection string 'SqlDbConnectionContainer' not found.");
 
